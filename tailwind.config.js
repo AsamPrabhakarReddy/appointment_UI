a627817ed1fe35a4a9a463e047ce9a36e10535c0
@Purnesh1226 <purnesh.kalavagunta@clouddatanetworks.com>
2024-12-27 13:09:18 +0530

    Major UI Changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      container: {},
+      colors: {
+        primaryColor: "#00684A",
+        blackColor: "#1A1A1A",
+      },
+    },
   },
   plugins: [],
-}
-
+};
