d96ac639a0b59f82650bdad26a792a1fe1268750
@Purnesh1226 <purnesh.kalavagunta@clouddatanetworks.com>
2024-12-27 15:22:27 +0530

    Another Round of UI Changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -101,43 +101,49 @@ const Hero = () => {
   return (
     <div className="w-full min-h-screen bg-gray-100 ">
       {/* Main Content */}
-      {/* <div className="text-3xl font-bold text-blue-900 mb-4 text-center p-4">
-        Mannam and Associates
-      </div> */}
-      <div className="pt-8 ">
-        <div className="max-w-[1400px] mx-auto flex flex-col sm:flex-row gap-4 p-4 bg-white shadow rounded">
-          {/* Side Menu */}
-          <div className="w-full sm:w-1/4 bg-gray-100 p-4 flex flex-col gap-4">
-            <ul className="space-y-4">
-              {sections.map((section, index) => (
-                <li
-                  key={section.id}
-                  className={`p-2 cursor-pointer rounded ${
-                    selectedIndex === index
-                      ? "bg-primaryColor text-white"
-                      : "bg-gray-300 text-gray-500"
-                  }`}
-                  onClick={() => handleIndex(index)}
-                >
-                  <div className="flex items-center gap-2">
-                    <span
-                      className={`p-2 rounded-full w-6 h-6 flex items-center justify-center font-bold ${
-                        selectedIndex === index
-                          ? "bg-white text-primaryColor"
-                          : "bg-gray-500 text-white"
-                      }`}
-                    >
-                      {index + 1}
-                    </span>
-                    <h1 className="text-md">{section.title}</h1>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
 
-          {/* Form Content */}
-          <div className="w-full sm:w-3/4">{renderFormContent()}</div>
+      <div className="lg:pt-8">
+        <div className="max-w-[1400px] mx-auto p-4 bg-white shadow rounded">
+          <div className="py-2 w-[100%] px-2 text-center">
+            <h1 className="text-2xl font-bold text-blackColor">
+              Mannam & Associates
+            </h1>
+          </div>
+          {/* Side Menu */}
+
+          <div className="flex flex-col lg:flex-row gap-4 ">
+            <div className="w-full lg:w-1/4 bg-gray-100 p-4 flex flex-col gap-4 shadow">
+              <ul className="space-y-4">
+                {sections.map((section, index) => (
+                  <li
+                    key={section.id}
+                    className={`lg:p-3 p-2 cursor-pointer rounded ${
+                      selectedIndex === index
+                        ? "bg-primaryColor text-white"
+                        : "bg-gray-300 text-gray-500"
+                    }`}
+                    onClick={() => handleIndex(index)}
+                  >
+                    <div className="flex items-center gap-2">
+                      <span
+                        className={`p-2 rounded-full w-6 h-6 flex items-center justify-center font-bold ${
+                          selectedIndex === index
+                            ? "bg-white text-primaryColor"
+                            : "bg-gray-500 text-white"
+                        }`}
+                      >
+                        {index + 1}
+                      </span>
+                      <h1 className="text-md">{section.title}</h1>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            {/* Form Content */}
+            <div className="w-full lg:w-3/4 shadow">{renderFormContent()}</div>
+          </div>
         </div>
 
         <div className="text-center text-xs">
