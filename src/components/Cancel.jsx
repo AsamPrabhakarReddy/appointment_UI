df837a6822ea6795c878bd1e85d3984baf801226
ASAM PRABHAKAR REDDY <asamprabhakarbala@gmail.com>
2025-01-09 18:21:51 +0530

    spell mistakes fixed

--- src/components/Cancel.jsx
+++ src/components/Cancel.jsx
@@ -94,7 +94,7 @@ const Cancel = () => {
       
                                 <!-- Title (left-aligned) -->
                                 <div style="width: 100%; text-align: center; margin-bottom: 20px;">
-                                  <h1 style="margin: 0; font-size: 25px;">Your Consultation Cancelled Successfulyl</h1>
+                                  <h1 style="margin: 0; font-size: 25px;">Your Consultation Cancelled Successfully</h1>
                                 </div>
                               </div>
       
