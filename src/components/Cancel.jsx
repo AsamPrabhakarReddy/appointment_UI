eece48e86c0720949c563008b1e85dca6735d4ad
ASAM PRABHAKAR REDDY <asamprabhakarbala@gmail.com>
2025-01-09 11:52:39 +0530

    removed summary functionality

--- src/components/Cancel.jsx
+++ src/components/Cancel.jsx
@@ -116,9 +116,7 @@ const Cancel = () => {
             });
     };
 
-    handleSummaryClick = ()=>{
-        navigate(`/cancel-reschedule/${appointmentId}`)
-    }
+    
 
   return (
     <div className='w-full h-screen bg-gray-400 p-4'>
@@ -199,7 +197,7 @@ const Cancel = () => {
     
                     <div className="flex justify-between w-[100%] p-4">
                         <button 
-                            onClick={handleSummaryClick}    
+                            // onClick={handleSummaryClick}    
                            className="px-6 py-2 flex flex-row items-center justify-center bg-primaryColor text-white rounded lg:w-1/3 "
                         >
                             <TiArrowLeft className="mr-2 text-2xl" />
